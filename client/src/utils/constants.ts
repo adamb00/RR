74ce0c5a47fb105e13cc74b42da18b5e39e4e119
borsodiadam <borsodi.dm@gmail.com>
2024-02-19 09:34:28 +0100

    url

--- client/src/utils/constants.ts
+++ client/src/utils/constants.ts
@@ -2,14 +2,17 @@ export const ITEM_PER_PAGE = 10;
 export const AFFILIATE_DEPTH = 3;
 export const FACEBOOK_APP_ID = '3092655644205329';
 
-export const BASE_URL_SOCKET = 'http://192.168.0.33:8000';
+// export const BASE_URL_SOCKET = 'http://192.168.0.33:8000';
+export const BASE_URL_SOCKET = 'https://rr-server.onrender.com'; // RENDER
 // export const BASE_URL_SOCKET = 'http://172.20.10.3:8000/'; // MOBILNET
 // export const BASE_URL_SOCKET = 'http://192.168.20.189:8000'; // BANDULA
 
-export const BASE_URL = 'http://192.168.0.33:8000/api/v1/';
+// export const BASE_URL = 'http://192.168.0.33:8000/api/v1/';
+export const BASE_URL = 'https://rr-server.onrender.com/api/v1'; // RENDER
 // export const BASE_URL = 'http://172.20.10.3:8000/api/v1/'; // MOBILNET
 // export const BASE_URL = 'http://192.168.20.189:8000/api/v1/'; // BANDULA
 
-export const BASE_URL_LINK = 'http://192.168.0.33:5173/';
+// export const BASE_URL_LINK = 'http://192.168.0.33:5173/';
+export const BASE_URL_LINK = 'https://rr-server.onrender.com/'; // RENDER
 // export const BASE_URL_LINK = 'http://172.20.10.3:5173/'; // MOBILNET
 // export const BASE_URL_LINK = 'http://192.168.20.189:5173/'; // BANDULA
