e1adda6c9fda25c25753b60163bbd020b44d01bd
borsodiadam <borsodi.dm@gmail.com>
2024-02-19 09:23:47 +0100

    Netlify

--- client/src/features/Account/UpdateUser.tsx
+++ client/src/features/Account/UpdateUser.tsx
@@ -1,7 +1,3 @@
-import { useForm } from 'react-hook-form';
-import UserInput from '../../ui/UserInteractions/UserInput';
-
 export default function UpdateUser() {
-   const { control } = useForm();
    return <div>{/* <UserInput control={control} name='name' /> */}</div>;
 }
