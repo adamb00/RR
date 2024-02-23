41760ebef5be7c2300a5ee0f98a01f9d3e2e4b76
borsodiadam <borsodi.dm@gmail.com>
2024-02-23 16:23:48 +0100

    Up

--- server/src/server.ts
+++ server/src/server.ts
@@ -14,8 +14,6 @@ mongoose.connect(DB).then(() => console.log('DB connection successful!'));
 
 const port = env.PORT || 3000;
 
-console.log('test');
-
 // const server = app.listen(port, () => {
 //    console.log(`App running on port ${port}...`);
 // });
