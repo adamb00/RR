66e2da74fc1acff3da8474e061636c1d1f4abd2b
borsodiadam <borsodi.dm@gmail.com>
2024-02-23 09:05:35 +0100

    Add .env programatically

--- client/src/utils/constants.ts
+++ client/src/utils/constants.ts
@@ -2,14 +2,14 @@ export const ITEM_PER_PAGE = 10;
 export const AFFILIATE_DEPTH = 3;
 export const FACEBOOK_APP_ID = '3092655644205329';
 
-export const BASE_URL_SOCKET = 'http://167.99.131.21:8000'; // LIVE
+export const BASE_URL_SOCKET = 'http://165.227.173.40:8000'; // LIVE
 // export const BASE_URL_SOCKET = 'http://192.168.0.33:8000';
 // export const BASE_URL_SOCKET = 'http://172.20.10.3:8000/'; // MOBILNET
 
-export const BASE_URL = `http://167.99.131.21:8000/api/v1/`; // LIVE
+export const BASE_URL = `http://165.227.173.40:8000/api/v1/`; // LIVE
 // export const BASE_URL = 'http://192.168.0.33:8000/api/v1/';
 // export const BASE_URL = 'http://172.20.10.3:8000/api/v1/'; // MOBILNET
 
-export const BASE_URL_LINK = 'http://167.99.131.21:5173/'; // LIVE
+export const BASE_URL_LINK = 'http://165.227.173.40:5173/'; // LIVE
 // export const BASE_URL_LINK = 'http://192.168.0.33:5173/';
 // export const BASE_URL_LINK = 'http://172.20.10.3:5173/'; // MOBILNET
