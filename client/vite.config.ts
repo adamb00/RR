d37c768f711915f3260b8c06af644203347d82d7
borsodiadam <borsodi.dm@gmail.com>
2024-02-24 09:06:08 +0100

    Update environment

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,21 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import dotenv from 'dotenv';
 
-export default defineConfig({
-   plugins: [react()],
+// export default defineConfig(({mode}) => {
+//    require('dotenv').config({ path: `./.env.${mode}` })
+//    plugins: [react()],
+// });
+
+export default defineConfig(({ mode }) => {
+   dotenv.config({ path: `./.env.${mode}` });
+   console.log(process.env.VITE_BASE_URL);
+   const env = loadEnv(mode, process.cwd(), '');
+   console.log('env', env);
+
+   return {
+      plugins: [react()],
+
+      root: 'src',
+   };
 });
