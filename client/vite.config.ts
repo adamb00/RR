bd57e940c23abf80fe034df068a5775fd4a84d30
borsodiadam <borsodi.dm@gmail.com>
2024-02-24 09:31:44 +0100

    Update environment

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,21 +1,20 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-import dotenv from 'dotenv';
 
-// export default defineConfig(({mode}) => {
-//    require('dotenv').config({ path: `./.env.${mode}` })
+// export default defineConfig({
 //    plugins: [react()],
 // });
 
 export default defineConfig(({ mode }) => {
-   dotenv.config({ path: `./.env.${mode}` });
-   console.log(process.env.VITE_BASE_URL);
    const env = loadEnv(mode, process.cwd(), '');
-   console.log('env', env);
-
    return {
+      define: {
+         'process.env.NODE_ENV': JSON.stringify(env.NODE_ENV),
+         'process.env.FACEBOOK_APP_ID': JSON.stringify(env.FACEBOOK_APP_ID),
+         'process.env.BASE_URL_SOCKET': JSON.stringify(env.BASE_URL_SOCKET),
+         'process.env.BASE_URL': JSON.stringify(env.BASE_URL),
+         'process.env.BASE_URL_LINK': JSON.stringify(env.BASE_URL_LINK),
+      },
       plugins: [react()],
-
-      root: 'src',
    };
 });
