acb0c7e3845fb111d6c66e1f54d075b9209420ea
borsodiadam <borsodi.dm@gmail.com>
2024-02-24 07:47:26 +0100

    Update environment

--- client/src/services/apiNotifications.ts
+++ client/src/services/apiNotifications.ts
@@ -1,8 +1,8 @@
-import { BASE_URL } from '../utils/constants';
+// import { BASE_URL } from '../utils/constants';
 import { OPTIONS } from '../utils/helpers';
 
 export const createNotification = async (data: object) => {
-   const response = await fetch(BASE_URL + 'notification', OPTIONS({ method: 'POST', data }));
+   const response = await fetch(import.meta.env.VITE_BASE_URL + 'notification', OPTIONS({ method: 'POST', data }));
    const responseData = await response.json();
 
    return responseData;
