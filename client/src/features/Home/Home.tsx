bc8c516c0d3e45f5a032c5bcf4583bd70e7e3397
borsodiadam <borsodi.dm@gmail.com>
2024-02-25 17:23:49 +0100

    Update environment

--- client/src/features/Home/Home.tsx
+++ client/src/features/Home/Home.tsx
@@ -6,10 +6,7 @@ import { selectCurrentUser } from '../Auth/slices/auth/authSlice';
 export default function Home() {
    const user = useSelector(selectCurrentUser);
 
-   console.log(import.meta.env.REACT_APP_NODE_ENV);
-
-   console.log('try', import.meta.env.REACT_APP_BASE_URL);
-   console.log('try', import.meta.env.REACT_APP_NODE_ENV);
+   console.log(import.meta.env.VITE_NODE_ENV);
 
    return <div className='home'>{user ? <HomeAuth /> : <HomeNoAuth />}</div>;
 }
