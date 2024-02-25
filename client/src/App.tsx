f467e4afaa4abd279e4636f6e1c382d6c8659e3b
borsodiadam <borsodi.dm@gmail.com>
2024-02-25 11:36:52 +0100

    Update environment

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -31,7 +31,6 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import HttpApi from 'i18next-http-backend';
-import 'dotenv/config';
 
 i18n
    .use(initReactI18next)
