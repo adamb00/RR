adc8337febc5ed238088bf59102a748b2ff9e028
borsodiadam <borsodi.dm@gmail.com>
2024-02-25 11:23:20 +0100

    Update environment

--- client/src/features/Home/Home.tsx
+++ client/src/features/Home/Home.tsx
@@ -6,8 +6,6 @@ import { selectCurrentUser } from '../Auth/slices/auth/authSlice';
 export default function Home() {
    const user = useSelector(selectCurrentUser);
 
-   console.log(process.env.BASE_URL);
-   console.log('vite', process.env.NODE_ENV);
    console.log('try', process.env.REACT_APP_BASE_URL);
    console.log('try', process.env.REACT_APP_NODE_ENV);
 
