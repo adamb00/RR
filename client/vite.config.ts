c321a15cfefd0929580819379081d84e20387231
borsodiadam <borsodi.dm@gmail.com>
2024-03-22 13:36:09 +0100

    Test

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -10,6 +10,15 @@ export default defineConfig({
          '@': path.resolve(__dirname, './src'),
       },
    },
+   server: {
+      proxy: {
+         '/api': {
+            target: 'https://r2byou.com:8000/api/v1',
+            changeOrigin: true,
+            secure: false,
+         },
+      },
+   },
    preview: {
       host: true,
       port: 3000, // client port when execute npm run preview
