951591d4b05b1e685160d45900e4cdc0748937f7
borsodiadam <borsodi.dm@gmail.com>
2024-03-23 08:36:19 +0100

    RouterOptions

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -8,7 +8,7 @@ import { StrictMode } from 'react';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 import { DndProvider } from 'react-dnd';
 import { store, persistor } from './app/store.ts';
-import { Routes, Route, HashRouter } from 'react-router-dom';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import 'drag-drop-touch';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
@@ -25,7 +25,7 @@ const queryClient = new QueryClient({
 
 createRoot(document.getElementById('root')!).render(
    <StrictMode>
-      <HashRouter>
+      <BrowserRouter>
          <QueryClientProvider client={queryClient}>
             <Provider store={store}>
                <LinkProvider>
@@ -42,6 +42,6 @@ createRoot(document.getElementById('root')!).render(
                </LinkProvider>
             </Provider>
          </QueryClientProvider>
-      </HashRouter>
+      </BrowserRouter>
    </StrictMode>
 );
