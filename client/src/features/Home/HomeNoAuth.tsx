7ad2e93b8aa3f8f558499a14b2db3c421d75f703
borsodiadam <borsodi.dm@gmail.com>
2024-04-08 13:13:00 +0200

    admin init

--- client/src/features/Home/HomeNoAuth.tsx
+++ client/src/features/Home/HomeNoAuth.tsx
@@ -7,20 +7,22 @@ export default function HomeNoAuth() {
       isDark = theme === 'dark';
 
    return (
-      <div className='home__no-auth'>
-         <ShareLinks />
-         <div className='home__no-auth--container'>
-            <SparklesCore
-               id='tsparticlesfullpage'
-               background='transparent'
-               minSize={0.6}
-               maxSize={1.4}
-               particleDensity={100}
-               className='w-full h-full'
-               particleColor={isDark ? '#8debcf' : '#ed535b'}
-            />
+      <>
+         <div className='home__no-auth'>
+            <div className='home__no-auth--container'>
+               <SparklesCore
+                  id='tsparticlesfullpage'
+                  background='transparent'
+                  minSize={0.6}
+                  maxSize={1.4}
+                  particleDensity={100}
+                  className='w-full h-full'
+                  particleColor={isDark ? '#8debcf' : '#ed535b'}
+               />
+            </div>
+            <h1 className='heading-primary'>Here comes some fancy title</h1>
          </div>
-         <h1 className='heading-primary'>Here comes some fancy title</h1>
-      </div>
+         <ShareLinks />
+      </>
    );
 }
