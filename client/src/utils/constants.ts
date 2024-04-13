51d89ac877b6be299f62a09af5cfb9f1e4ff4009
borsodiadam <borsodi.dm@gmail.com>
2024-04-13 09:22:43 +0200

    Sockets removed

--- client/src/utils/constants.ts
+++ client/src/utils/constants.ts
@@ -1,10 +1,7 @@
-import { io } from 'socket.io-client';
-
 export const iconSize = 44;
 export const iconSizeMobile = 32;
 export const ITEM_PER_PAGE = 10;
 export const AFFILIATE_DEPTH = 3;
-export const socket = io(import.meta.env.VITE_BASE_URL_SOCKET, { path: '/socket.io' });
 
 export const primaryColor = 'rgb(237, 83, 91)';
 export const secondaryColor = 'rgb(192, 244, 229)';
