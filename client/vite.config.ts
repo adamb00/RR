85b30283c29d97f14a3727f685cfabb70d496a0c
borsodiadam <borsodi.dm@gmail.com>
2024-04-18 09:22:01 +0200

    test

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,25 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import viteCompression from 'vite-plugin-compression';
+// import viteCompression from 'vite-plugin-compression';
 import path from 'path';
 
 export default defineConfig({
-   plugins: [react(), viteCompression()],
+   plugins: [react()],
    resolve: {
       alias: {
          '@': path.resolve(__dirname, './src'),
       },
-   },
-   server: {
-      watch: {
-         usePolling: true,
-      },
-      host: true,
-      strictPort: true,
-      port: 3000,
-   },
-   preview: {
-      port: 3000,
-      host: true,
    },
 });
