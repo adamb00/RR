abc71dd997d4dc92f167410efc05fccc22255cd4
borsodiadam <borsodi.dm@gmail.com>
2024-04-19 08:06:14 +0200

    test

--- client/src/utils/TestComp.tsx
+++ client/src/utils/TestComp.tsx
@@ -3,7 +3,7 @@ import { useForm } from 'react-hook-form';
 export default function TestComp() {
    const { handleSubmit } = useForm();
    const handleOnSubmit = async () => {
-      const res = await fetch(import.meta.env.VITE_BASEURL + '/user/mark-notifications');
+      const res = await fetch('https://r2byou.com/api/v1/user/mark-notifications');
       console.log(res);
    };
    return <button onClick={handleSubmit(handleOnSubmit)}>Press</button>;
