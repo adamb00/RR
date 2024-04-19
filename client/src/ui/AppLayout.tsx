1445c4aef04b7f3009e232d48f290f2c5279b0b3
borsodiadam <borsodi.dm@gmail.com>
2024-04-19 07:53:21 +0200

    test

--- client/src/ui/AppLayout.tsx
+++ client/src/ui/AppLayout.tsx
@@ -1,37 +1,40 @@
 // import { Outlet, useParams } from 'react-router-dom';
 // import Header from './Header';
 // import { useIsNotification } from '../hooks/useIsNotification';
-// // import { socket } from '@/utils/constants';
-// // import { useEffect } from 'react';
-// // import { useRefreshUserMutation } from '@/features/Auth/slices/auth/authApiSlice';
-// // import { useDispatch } from 'react-redux';
-// // import { updateUser } from '@/features/Auth/slices/auth/authSlice';
+// import { socket } from '@/utils/constants';
+// import { useEffect } from 'react';
+// import { useRefreshUserMutation } from '@/features/Auth/slices/auth/authApiSlice';
+// import { useDispatch } from 'react-redux';
+// import { updateUser } from '@/features/Auth/slices/auth/authSlice';
 
-// export default function AppLayout() {
-//    const { username } = useParams();
-//    const { isNotification } = useIsNotification();
-//    const hasDynamicId = !!username;
-//    // const [refreshUser] = useRefreshUserMutation();
-//    // const dispatch = useDispatch();
+import TestComp from '@/utils/TestComp';
 
-//    // useEffect(() => {
-//    //    const handleNotification = async () => {
-//    //       const res = await refreshUser({}).unwrap();
-//    //       dispatch(updateUser(res.data));
-//    //    };
+export default function AppLayout() {
+   // const { username } = useParams();
+   // const { isNotification } = useIsNotification();
+   // const hasDynamicId = !!username;
+   // const [refreshUser] = useRefreshUserMutation();
+   // const dispatch = useDispatch();
 
-//    //    socket.on('notification_created', handleNotification);
+   // useEffect(() => {
+   //    const handleNotification = async () => {
+   //       const res = await refreshUser({}).unwrap();
+   //       dispatch(updateUser(res.data));
+   //    };
 
-//    //    return () => {
-//    //       socket.off('notification_created', handleNotification);
-//    //    };
-//    // }, [dispatch, refreshUser]);
-//    return (
-//       <>
-//          <main className='main'>
-//             {(!hasDynamicId || isNotification) && <Header />}
-//             <Outlet />
-//          </main>
-//       </>
-//    );
-// }
+   //    socket.on('notification_created', handleNotification);
+
+   //    return () => {
+   //       socket.off('notification_created', handleNotification);
+   //    };
+   // }, [dispatch, refreshUser]);
+   return (
+      <>
+         <TestComp />
+         {/* <main className='main'>
+            {(!hasDynamicId || isNotification) && <Header />}
+            <Outlet />
+         </main> */}
+      </>
+   );
+}
