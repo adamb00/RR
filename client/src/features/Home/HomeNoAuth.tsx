b5a8302bdd0ea8bd6a983d68c729c1b37904284a
borsodiadam <borsodi.dm@gmail.com>
2024-04-19 07:59:02 +0200

    test

--- client/src/features/Home/HomeNoAuth.tsx
+++ client/src/features/Home/HomeNoAuth.tsx
@@ -1,26 +1,28 @@
-import { useTheme } from '@/context/ThemeContext';
-import { SparklesCore } from '@/ui/Aceternity/Sparkles';
+// import { useTheme } from '@/context/ThemeContext';
+// import { SparklesCore } from '@/ui/Aceternity/Sparkles';
+import TestComp from '@/utils/TestComp';
 
 export default function HomeNoAuth() {
-   const { theme } = useTheme(),
-      isDark = theme === 'dark';
+   // const { theme } = useTheme(),
+   //    isDark = theme === 'dark';
 
    return (
-      <>
-         <div className='home__no-auth'>
-            <div className='home__no-auth--container'>
-               <SparklesCore
-                  id='tsparticlesfullpage'
-                  background='transparent'
-                  minSize={0.6}
-                  maxSize={1.4}
-                  particleDensity={100}
-                  className='w-full h-full'
-                  particleColor={isDark ? '#8debcf' : '#ed535b'}
-               />
-            </div>
-            <h1 className='heading-primary'>Here comes some fancy title</h1>
-         </div>
-      </>
+      <TestComp />
+      // <>
+      //    <div className='home__no-auth'>
+      //       <div className='home__no-auth--container'>
+      //          <SparklesCore
+      //             id='tsparticlesfullpage'
+      //             background='transparent'
+      //             minSize={0.6}
+      //             maxSize={1.4}
+      //             particleDensity={100}
+      //             className='w-full h-full'
+      //             particleColor={isDark ? '#8debcf' : '#ed535b'}
+      //          />
+      //       </div>
+      //       <h1 className='heading-primary'>Here comes some fancy title</h1>
+      //    </div>
+      // </>
    );
 }
--- client/src/ui/AppLayout.tsx
+++ client/src/ui/AppLayout.tsx
@@ -1,18 +1,16 @@
-// import { Outlet, useParams } from 'react-router-dom';
-// import Header from './Header';
-// import { useIsNotification } from '../hooks/useIsNotification';
+import { Outlet, useParams } from 'react-router-dom';
+import Header from './Header';
+import { useIsNotification } from '../hooks/useIsNotification';
 // import { socket } from '@/utils/constants';
 // import { useEffect } from 'react';
 // import { useRefreshUserMutation } from '@/features/Auth/slices/auth/authApiSlice';
 // import { useDispatch } from 'react-redux';
 // import { updateUser } from '@/features/Auth/slices/auth/authSlice';
 
-import TestComp from '@/utils/TestComp';
-
 export default function AppLayout() {
-   // const { username } = useParams();
-   // const { isNotification } = useIsNotification();
-   // const hasDynamicId = !!username;
+   const { username } = useParams();
+   const { isNotification } = useIsNotification();
+   const hasDynamicId = !!username;
    // const [refreshUser] = useRefreshUserMutation();
    // const dispatch = useDispatch();
 
@@ -30,11 +28,10 @@ export default function AppLayout() {
    // }, [dispatch, refreshUser]);
    return (
       <>
-         <TestComp />
-         {/* <main className='main'>
+         <main className='main'>
             {(!hasDynamicId || isNotification) && <Header />}
             <Outlet />
-         </main> */}
+         </main>
       </>
    );
 }
