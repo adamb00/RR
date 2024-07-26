75b918ed31a2ffa75cd90feee221efbf76b7459c
borsodiadam <borsodi.dm@gmail.com>
2024-07-26 18:27:13 +0200

    Links modified

--- client/src/features/Account/UploadImage.tsx
+++ client/src/features/Account/UploadImage.tsx
@@ -1,61 +1,61 @@
-import { useForm } from 'react-hook-form';
-import UserImageInput from '@/ui/UserInteractions/UserImageInput';
-import { useUploadUserImageMutation } from '@/features/Auth/slices/user/userApiSlice';
-import { useState } from 'react';
-import { useAppDispatch } from '@/redux-hooks';
+// import { useForm } from 'react-hook-form';
+// import UserImageInput from '@/ui/UserInteractions/UserImageInput';
+// import { useUploadUserImageMutation } from '@/features/Auth/slices/user/userApiSlice';
+// import { useState } from 'react';
+// import { useAppDispatch } from '@/redux-hooks';
 
-import { selectCurrentUser, updateUser } from '@/features/Auth/slices/auth/authSlice';
-import { useSelector } from 'react-redux';
-import Loader from '@/ui/Loader';
-import { userImage } from '@/utils/helpers';
-// import { useGetImage } from '@/hooks/useGetImage';
+// import { selectCurrentUser, updateUser } from '@/features/Auth/slices/auth/authSlice';
+// import { useSelector } from 'react-redux';
+// import Loader from '@/ui/Loader';
+// import { userImage } from '@/utils/helpers';
+// // import { useGetImage } from '@/hooks/useGetImage';
 
-export default function UploadImage() {
-   const [uploading, setUploading] = useState(false);
-   const { control } = useForm();
-   const [uploadUserImageApi] = useUploadUserImageMutation();
-   const dispatch = useAppDispatch();
-   const user = useSelector(selectCurrentUser);
+// export default function UploadImage() {
+//    const [uploading, setUploading] = useState(false);
+//    const { control } = useForm();
+//    const [uploadUserImageApi] = useUploadUserImageMutation();
+//    const dispatch = useAppDispatch();
+//    const user = useSelector(selectCurrentUser);
 
-   // const { image: userImage, isLoading: isLoadingUserImage } = useGetImage(user);
-   const handleImageChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
-      const image = event.target.files?.[0];
+//    // const { image: userImage, isLoading: isLoadingUserImage } = useGetImage(user);
+//    const handleImageChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
+//       const image = event.target.files?.[0];
 
-      if (image && !uploading) {
-         setUploading(true);
-         const formData = new FormData();
-         formData.append('image', image);
+//       if (image && !uploading) {
+//          setUploading(true);
+//          const formData = new FormData();
+//          formData.append('image', image);
 
-         if (formData.has('image')) {
-            try {
-               const res = await uploadUserImageApi(formData).unwrap();
-               dispatch(updateUser({ ...res.data.user }));
-            } catch (error) {
-               console.error('Error uploading image:', error);
-            } finally {
-               setUploading(false);
-            }
-         }
-      }
-   };
+//          if (formData.has('image')) {
+//             try {
+//                const res = await uploadUserImageApi(formData).unwrap();
+//                dispatch(updateUser({ ...res.data.user }));
+//             } catch (error) {
+//                console.error('Error uploading image:', error);
+//             } finally {
+//                setUploading(false);
+//             }
+//          }
+//       }
+//    };
 
-   return (
-      <UserImageInput
-         control={control}
-         name='image'
-         className='upload-image__container'
-         onChange={handleImageChange}
-         id={user._id}
-      >
-         <label className='upload-image' htmlFor={user._id}>
-            {uploading ? (
-               <Loader size={100} />
-            ) : user.photo ? (
-               <img src={userImage(user.photo)} alt='User Image' className='upload-image__image' />
-            ) : (
-               '+'
-            )}
-         </label>
-      </UserImageInput>
-   );
-}
+//    return (
+//       <UserImageInput
+//          control={control}
+//          name='image'
+//          className='upload-image__container'
+//          onChange={handleImageChange}
+//          id={user._id}
+//       >
+//          <label className='upload-image' htmlFor={user._id}>
+//             {uploading ? (
+//                <Loader size={100} />
+//             ) : user.photo ? (
+//                <img src={userImage(user.photo)} alt='User Image' className='upload-image__image' />
+//             ) : (
+//                '+'
+//             )}
+//          </label>
+//       </UserImageInput>
+//    );
+// }
