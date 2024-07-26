b004ad8ea88fef2748d9d37ebc32d1fd7d9b5be8
borsodiadam <borsodi.dm@gmail.com>
2024-07-26 17:36:31 +0200

    Links modified

--- admin/src/hooks/useGetImage.ts
+++ admin/src/hooks/useGetImage.ts
@@ -1,40 +1,40 @@
-import { useEffect, useState } from 'react';
-import { getImage } from '@/services/apiImage';
-import { UserProfileData } from '@/interfaces/IUser';
-import { ILink } from '@/interfaces/ILink';
+// import { useEffect, useState } from 'react';
+// import { getImage } from '@/services/apiImage';
+// import { UserProfileData } from '@/interfaces/IUser';
+// import { ILink } from '@/interfaces/ILink';
 
-type ResourceData = UserProfileData | ILink;
+// type ResourceData = UserProfileData | ILink;
 
-export const useGetImage = <T extends ResourceData>(data: T | undefined) => {
-   const [image, setImage] = useState<string | undefined>(data ? getImagePath(data) : undefined);
-   const [isLoading, setIsLoading] = useState<boolean>(false);
-   useEffect(() => {
-      const fetchDataImage = async () => {
-         try {
-            const imagePath = getImagePath(data);
-            if (data && imagePath) {
-               setIsLoading(true);
-               const fetchedImage = await getImage(imagePath);
-               setImage(fetchedImage);
-            }
-         } catch (err) {
-            console.log(err);
-         } finally {
-            setIsLoading(false);
-         }
-      };
+// export const useGetImage = <T extends ResourceData>(data: T | undefined) => {
+//    const [image, setImage] = useState<string | undefined>(data ? getImagePath(data) : undefined);
+//    const [isLoading, setIsLoading] = useState<boolean>(false);
+//    useEffect(() => {
+//       const fetchDataImage = async () => {
+//          try {
+//             const imagePath = getImagePath(data);
+//             if (data && imagePath) {
+//                setIsLoading(true);
+//                const fetchedImage = await getImage(imagePath);
+//                setImage(fetchedImage);
+//             }
+//          } catch (err) {
+//             console.log(err);
+//          } finally {
+//             setIsLoading(false);
+//          }
+//       };
 
-      fetchDataImage();
-   }, [data]);
+//       fetchDataImage();
+//    }, [data]);
 
-   return { image, isLoading };
-};
+//    return { image, isLoading };
+// };
 
-const getImagePath = (data: UserProfileData | ILink | undefined) => {
-   if (data && 'photo' in data) {
-      return data.photo;
-   } else if (data && 'image' in data) {
-      return data.image;
-   }
-   return undefined;
-};
+// const getImagePath = (data: UserProfileData | ILink | undefined) => {
+//    if (data && 'photo' in data) {
+//       return data.photo;
+//    } else if (data && 'image' in data) {
+//       return data.image;
+//    }
+//    return undefined;
+// };
